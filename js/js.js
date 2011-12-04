644778224362688a080c3f3e5968b3865465e89e
Riccardo Strobbia <holdwine@gmail.com>
2011-12-04 17:18:37 +0100

    Added display for amount values

--- js/js.js
+++ js/js.js
@@ -1,20 +1,41 @@
-var jq = jQuery, amt = 0, sliders_num = 3, max = 100, sliders = get_sliders(), sliders_num = sliders.length, timeout, time = 0;
+var jq = jQuery, amt = 0, sliders_num = 3, max = 100, sliders = get_sliders(), sliders_num = sliders.length, timeout, time = 0, defaults = new Defaults();
 
 jq(function(){
 	nonna_distibutePaymentInit( jq('#em-booking-form') );
 });
 
+function Defaults(){
+	this.split = {
+		'dev' : 70,
+		'cook' : 20,
+		'charity' : 10
+	},
+	this.tmp = {
+		'dev': 0,
+		'cook' : 0,
+		'charity' : 0
+	}
+};
+
 function nonna_distibutePaymentInit( cont )
 {	
 	if( jq('input[name="donate"]').is('input') )
 	{
-		var container = cont, distibute_container = jq('<div id="distibute-credit-container" class="booking-box">'), amount_input = jq('input[name="donate"]');
+		var container = cont, distibute_container = jq('<div id="distibute-credit-container" class="booking-box">'), amount_input = jq('input[name="donate"]'), display_prices = new Array(), append = jq('.em-booking-buttons');
 		
-		container.append( distibute_container );
+		jq('div.em-booking-buttons').before( distibute_container );
 		
 		jq.each(sliders, function(i){
 			var name = jq(this).attr('id').split('_')[1];
+			
+			display_prices[i] = jq('<div class="display-splits" id="split_'+name+'">');
+			
+			display_prices[i].text(0);
+			
 			distibute_container.append( jq(this) );
+			
+			jq(this).after(display_prices[i]);
+			
 			jq( this ).slider({
 				min:1,
 				max: max,
@@ -22,19 +43,34 @@ function nonna_distibutePaymentInit( cont )
 				step: 1,
 				start: function( event, ui )
 				{
-					
+					///
 				},
 				slide: function(event, ui) 
 				{ 
-					return slideslide(event,ui,i);
+					slideslide(event,ui,i);
+					return updatePrices( display_prices );
+				},
+				stop: function(event, ui)
+				{
+					/// 
 				}
 			});
 		});
 		
 		amount_input.blur(function(e){
 			amt = jq(this).val();
+			updatePrices( display_prices );
 		});
 	} 
+};
+function updatePrices( elements )
+{
+	var els = elements;
+	jq.each(els, function(i){
+		var /*name = els[i].attr('id').split('_')[1],*/ rate = sliders[i].slider('value');
+		money = ( amt / 100 ) * rate;
+		elements[i].text( prettymoney(money) );
+	});
 };
 function moveothersliders(index,val) {
   var sum = 0;
@@ -42,10 +78,12 @@ function moveothersliders(index,val) {
     if (i == index) continue;
     sum += sliders[i].slider('value');
   }
-  var mult = (max - (val ? val : sliders[index].slider('value'))) / sum;
+  var mult = (max - (val ? val : sliders[index].slider('value'))) / sum, name, val;
   for (var i = 0; i<sliders_num; i++) {
     if (i == index) continue;
-    sliders[i].slider('value',sliders[i].slider('value') * mult);
+	name = sliders[i].attr('id').split('_')[1];
+	val = sliders[i].slider('value') * mult;
+    sliders[i].slider('value',val);
   }
 };
 var slideslide = function(e,ui,index) {
@@ -58,14 +96,6 @@ var slideslide = function(e,ui,index) {
   time = e.timeStamp;
   moveothersliders(index,ui.value);
 };
-var defaults = {
-	'split' : {
-		'dev' : 70,
-		'cook' : 20,
-		'charity' : 10
-	}
-};
-
 function get_sliders()
 {
 	var sliders = new Array( jq('<div id="slider_dev" class="sliders">'), jq('<div id="slider_cook" class="sliders">'), jq('<div id="slider_charity" class="sliders">') );
@@ -84,4 +114,11 @@ function cleanamount(a) {
   a = a.replace(',','');
   a = parseFloat(a);
   return a;
+};
+function prettymoney(c) {
+  c = '' + c;
+  c = c.split('.');
+  c.push('00');
+  if (c[1].length == 1) c[1] += '0';
+  return c[0] + '.' + c[1];
 };
