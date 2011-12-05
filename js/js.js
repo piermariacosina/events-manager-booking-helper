8ff1e2bd5dc4df5f8e2f7f6dc4e4f4c1b694c142
Riccardo Strobbia <holdwine@gmail.com>
2011-12-05 15:22:58 +0100

    Controls to round splitter money

--- js/js.js
+++ js/js.js
@@ -1,26 +1,32 @@
 var jq = jQuery, amt = 0, sliders_num = 3, max = 100, sliders = get_sliders(), sliders_num = sliders.length, timeout, time = 0, defaults = new Defaults();
 
 jq(function(){
+	if( jq('input[name="donate"]').is('input') ){
+
+		jq( "#messages_to_user" ).dialog({
+			autoOpen: false,
+			close:function(event, ui)
+			{
+				jq('input[name="donate"]').val('');
+			}
+		});
+	}
 	nonna_distibutePaymentInit( jq('#em-booking-form') );
 });
 
-function Defaults(){
+function Defaults()
+{
 	this.split = {
 		'dev' : 70,
 		'cook' : 20,
 		'charity' : 10
-	},
-	this.tmp = {
-		'dev': 0,
-		'cook' : 0,
-		'charity' : 0
 	}
 };
 function nonna_distibutePaymentInit( cont )
 {	
 	if( jq('input[name="donate"]').is('input') )
 	{
-		var container = cont, distibute_container = jq('<div id="distibute-credit-container" class="booking-box">'), amount_input = jq('input[name="donate"]'), display_prices = new Array(), append = jq('.em-booking-buttons');
+		var container = cont, distibute_container = jq('<div id="distibute-credit-container" class="booking-box">'), amount_input = jq('input[name="donate"]'), display_prices = new Array(), append = jq('.em-booking-buttons'), hiddens = new Array();
 		
 		
 		jq('div.em-booking-buttons').before( distibute_container );
@@ -32,7 +38,11 @@ function nonna_distibutePaymentInit( cont )
 			
 			display_prices[i].text('0.00');
 			
+			hiddens[i] = jq('<input type="hidden" name="split_amount_'+name+'" id="split_amount_'+name+'" value="0" />');
+			
 			distibute_container.append( jq(this) );
+			
+			container.append( hiddens[i] );
 			
 			jq(this).after(display_prices[i]);
 			
@@ -48,8 +58,12 @@ function nonna_distibutePaymentInit( cont )
 				slide: function(event, ui) 
 				{ 
 					slideslide(event,ui,i);
-					if( amt == 0 ) return;
-					return updatePrices( display_prices );
+					if( amt == 0 ) 
+					{
+						globalMessages('You should fill a donation amount to split it!');
+						return false;
+					}
+					return updatePrices( display_prices, hiddens, i );
 				},
 				stop: function(event, ui)
 				{
@@ -59,6 +73,7 @@ function nonna_distibutePaymentInit( cont )
 		});
 		
 		amount_input.keyup(function(e){
+			
 			var val = isNaN( jq(this).val() ) ? false : jq(this).val() ;
 
 			if(val)
@@ -67,7 +82,7 @@ function nonna_distibutePaymentInit( cont )
 
 				if( validamount( amt ) )
 				{
-					updatePrices( display_prices );
+					updatePrices( display_prices, hiddens );
 				}
 				else
 				{
@@ -81,13 +96,24 @@ function nonna_distibutePaymentInit( cont )
 		});
 	} 
 };
-function updatePrices( elements )
+function updatePrices( elements, hiddens, index )
 {
-	var els = elements;
+	var els = elements, pos = ( index ) ? index : 0;
+	
 	jq.each(els, function(i){
-		var /*name = els[i].attr('id').split('_')[1],*/ rate = sliders[i].slider('value');
+		var /*name = els[i].attr('id').split('_')[1],*/ rate = sliders[i].slider('value'), round = 0;
 		money = ( amt / 100 ) * rate;
-		elements[i].text( prettymoney(money) );
+		round = money.roundNumber(2);
+		if( round < 1 )
+		{
+			round = 0;
+		}
+		if( ( max -sliders[index].slider('value') ) < 1 )
+		{
+			round = parseInt( round );
+		}
+		elements[i].text( round );
+		hiddens[i].val( round );
 	});
 };
 function moveothersliders(index,val) {
@@ -119,7 +145,6 @@ function get_sliders()
 	var sliders = new Array( jq('<div id="slider_dev" class="sliders">'), jq('<div id="slider_cook" class="sliders">'), jq('<div id="slider_charity" class="sliders">') );
 	return sliders;
 };
-
 function validamount(a) {
   if (typeof(a) == 'number') return true;
   a = a.replace('$','');
@@ -132,16 +157,4 @@ function cleanamount(a) {
   a = a.replace(',','');
   a = parseFloat(a);
   return a;
-};
-function prettymoney(c) {
-  c = '' + c;
-  c = c.split('.');
-  c.push('00');
-  if (c[1].length == 1) c[1] += '0';
-  return c[0] + '.' + c[1];
-};
-String.prototype.isInteger = function()
-{
-	var reInteger = /^\d+$/;
-	return reInteger.test(this);
 };
